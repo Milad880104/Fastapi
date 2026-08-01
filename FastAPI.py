0ce56d60932eb0a299315a4a84f3c7a5926a697f
milad <milad.jafari1998@outlook.com>
2026-08-01 02:20:12 -0700

    updateing cod

--- FastAPI.py
+++ FastAPI.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+my_list=[{"id":1, "name":"Eli"}, 
+         {"id":2, "name":"Milad"},
+         {"id":3, "name":"jadi"}]
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+@app.get("/nemes")
+def list_names():
+    return my_list
+@app.get("/names/{name_id}")
+def chek_name(name_id):
+    for name in my_list:
+       if name["id"]==name_id:
+            return
+    
