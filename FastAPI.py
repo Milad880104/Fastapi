497426b6979cc5ad6e437e7846367e61eb200a2f
milad <milad.jafari1998@outlook.com>
2026-08-02 02:14:51 -0700

    adding query and status cod to project

--- FastAPI.py
+++ FastAPI.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, status, HTTPException
 import random
 app = FastAPI()
 my_list=[{"id":1, "name":"Eli"}, 
@@ -9,34 +9,38 @@ my_list=[{"id":1, "name":"Eli"},
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/names")
+@app.post("/names",status_code=status.HTTP_201_CREATED)
 def add_name(name):
     obj={"id":random.randint(4,100) ,"name":name}
     my_list.append(obj)
     return obj
 
-@app.get("/nemes")
-def list_names():
-    return my_list
-@app.get("/names/{name_id}")
+@app.get("/nemes",status_code=status.HTTP_302_FOUND)
+def list_names(q :str| None=Query(alias="search", default=None, max_length=20)):
+    for item in my_list:
+        if item["name"]==q:
+            return item
+    else: return my_list
+    
+@app.get("/names/{name_id}",status_code=status.HTTP_200_OK)
 def chek_name(name_id):
     for name in my_list:
        if name["id"]==name_id:
             return
     
 
-@app.put("/names/{name_id}")
+@app.put("/names/{name_id}", status_code=status.HTTP_201_CREATED)
 def up_name(name_id:int,item:str):
     for oskol in my_list:
         if oskol["id"]==name_id:
             oskol["name"]=item
             return oskol
 
-@app.delete("/names/{name_id}")
+@app.delete("/names/{name_id}",status_code=status.HTTP_204_NO_CONTENT)
 def del_item(name_id: int):
     for name in my_list:
         if name["id"]==name_id:
             my_list.remove(name)
-            return "item delete"
-    return "item not found"
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="item delete")
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="item not found")
 
