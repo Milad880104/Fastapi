1c06f71e572575354c74d0ae17f1fee4cebc4c87
milad <milad.jafari1998@outlook.com>
2026-08-01 23:57:12 -0700

    adding the some prameter (del,pul,post,get)

--- FastAPI.py
+++ FastAPI.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import random
 app = FastAPI()
 my_list=[{"id":1, "name":"Eli"}, 
          {"id":2, "name":"Milad"},
@@ -8,6 +8,13 @@ my_list=[{"id":1, "name":"Eli"},
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/names")
+def add_name(name):
+    obj={"id":random.randint(4,100) ,"name":name}
+    my_list.append(obj)
+    return obj
+
 @app.get("/nemes")
 def list_names():
     return my_list
@@ -17,3 +24,19 @@ def chek_name(name_id):
        if name["id"]==name_id:
             return
     
+
+@app.put("/names/{name_id}")
+def up_name(name_id:int,item:str):
+    for oskol in my_list:
+        if oskol["id"]==name_id:
+            oskol["name"]=item
+            return oskol
+
+@app.delete("/names/{name_id}")
+def del_item(name_id: int):
+    for name in my_list:
+        if name["id"]==name_id:
+            my_list.remove(name)
+            return "item delete"
+    return "item not found"
+
